5694538c54787c5c3a559698d4d8a343c8aa4461
Tadas <tadaspaplauskas@gmail.com>
2017-06-17 16:18:38 +0300

    Move configuration to environment

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {};
+
+config.mongodb = {
+    url: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/remindbot'
+};
+config.web = {
+    port: process.env.WEB_PORT || 80
+};
+
+config.messenger = {
+    token: process.env.MESSENGER_TOKEN,
+    url: 'https://graph.facebook.com/v2.6/'
+};
+
+module.exports = config;
